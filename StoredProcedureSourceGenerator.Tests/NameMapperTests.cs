c02380b6b89be37b3e83e99c0caaf062d5337e0a
Andrii Kurdiumov <kant2002@gmail.com>
2021-03-04 23:33:27 +0600

    Initial project implementation

---
+++ StoredProcedureSourceGenerator.Tests/NameMapperTests.cs
@@ -0,0 +1,26 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NameMapperTests.cs" company="Andrii Kurdiumov">
+// Copyright (c) Andrii Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace StoredProcedureSourceGenerator.Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class NameMapperTests
+    {
+        [DataTestMethod]
+        [DataRow("personId", "person_id")]
+        public void MyTestMethod(string parameterName, string expectedStoredProcedureParameter)
+        {
+            var storedProcedureParameter = NameMapper.MapName(parameterName);
+
+            Assert.AreEqual(expectedStoredProcedureParameter, storedProcedureParameter);
+        }
+    }
+}
---
+++ StoredProcedureSourceGenerator/NameMapper.cs
@@ -0,0 +1,29 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="NameMapper.cs" company="Andrii Kurdiumov">
+// Copyright (c) Andrii Kurdiumov. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+
+namespace StoredProcedureSourceGenerator
+{
+    using System.Linq;
+    using System.Text.RegularExpressions;
+
+    /// <summary>
+    /// Provides mapping name support between parameter names in C# to Stored procedures parameter names.
+    /// </summary>
+    public static class NameMapper
+    {
+        /// <summary>
+        /// Maps parameter name to the stored procedure name.
+        /// </summary>
+        /// <param name="parameterName">Name of the parameter to map.</param>
+        /// <returns>Corresponding stored procedure parameter name.</returns>
+        public static string MapName(string parameterName)
+        {
+            var firstname = Regex.Match(parameterName, "[^A-Z]*").Value;
+            var matches = Regex.Matches(parameterName, "[A-Z][^A-Z]*").Cast<Match>().Select(_ => _.Value.ToLower());
+            return string.Join("_", new string[] { firstname }.Union(matches));
+        }
+    }
+}
