58214244bd9ee6e3ba051459edec66bf2c65bcfb
Andrii Kurdiumov <kant2002@gmail.com>
2021-05-10 13:17:13 +0600

    Refactor tests

--- SqlMarshal.Tests/StoredProcedureGenerationTests.cs
+++ SqlMarshal.Tests/StoredProcedureGenerationTests.cs
@@ -6,16 +6,11 @@
 
 namespace SqlMarshal.Tests
 {
-    using System;
-    using System.Collections.Generic;
-    using System.Linq;
-    using System.Reflection;
     using Microsoft.CodeAnalysis;
-    using Microsoft.CodeAnalysis.CSharp;
     using Microsoft.VisualStudio.TestTools.UnitTesting;
 
     [TestClass]
-    public class StoredProcedureGenerationTests
+    public class StoredProcedureGenerationTests : CodeGenerationTestBase
     {
         [TestMethod]
         public void MapResultSetToProcedure()
@@ -1298,41 +1293,6 @@ namespace Foo
     }
 }";
             Assert.AreEqual(expectedOutput, output);
-        }
-
-        private string GetGeneratedOutput(string source, NullableContextOptions nullableContextOptions)
-        {
-            var syntaxTree = CSharpSyntaxTree.ParseText(source);
-
-            var references = new List<MetadataReference>();
-            Assembly[] assemblies = AppDomain.CurrentDomain.GetAssemblies();
-            foreach (var assembly in assemblies)
-            {
-                if (!assembly.IsDynamic && !string.IsNullOrWhiteSpace(assembly.Location))
-                {
-                    references.Add(MetadataReference.CreateFromFile(assembly.Location));
-                }
-            }
-
-            var compilation = CSharpCompilation.Create(
-                "foo",
-                new SyntaxTree[] { syntaxTree },
-                references,
-                new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary, nullableContextOptions: nullableContextOptions));
-
-            // var compileDiagnostics = compilation.GetDiagnostics();
-            // Assert.IsFalse(compileDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + compileDiagnostics.FirstOrDefault()?.GetMessage());
-            ISourceGenerator generator = new Generator();
-
-            var driver = CSharpGeneratorDriver.Create(generator);
-            driver.RunGeneratorsAndUpdateCompilation(compilation, out var outputCompilation, out var generateDiagnostics);
-            Assert.IsFalse(generateDiagnostics.Any(d => d.Severity == DiagnosticSeverity.Error), "Failed: " + generateDiagnostics.FirstOrDefault()?.GetMessage());
-
-            string output = outputCompilation.SyntaxTrees.Last().ToString();
-
-            Console.WriteLine(output);
-
-            return output;
         }
     }
 }
